fc07337bd6691705d24eeb46f13ff2a64f3fdbed
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-03-27 00:00:42 +0530

    validate login form

--- utils/validations/authFormValidate.js
+++ utils/validations/authFormValidate.js
@@ -1,12 +1,12 @@
 export const validateRegisterForm = (formData) => {
   const errors = {};
 
-  if (!formData.username.trim()) {
-    errors.username = "Username is required";
-  } else if (formData.username < 4) {
+  if (!formData.name.trim()) {
+    errors.name = "Username is required";
+  } else if (formData.name < 4) {
     errors.password = "Username must be at least 4 characters long";
-  } else if (!isValidUsername(formData.username)) {
-    errors.username =
+  } else if (!isValidUsername(formData.name)) {
+    errors.name =
       'Username can only contain letters, numbers, and underscores"';
   }
 
@@ -23,6 +23,25 @@ export const validateRegisterForm = (formData) => {
   } else if (!isValidPassword(formData.password)) {
     errors.password =
       "Password must contain at uppercase and lowercase letters, digits, and special character";
+  }
+
+  return errors;
+};
+
+export const validateLoginForm = (formData) => {
+  const errors = {};
+  if (!formData.email.trim()) {
+    errors.email = "Email is required";
+  } else if (!isValidEmail(formData.email)) {
+    errors.email = "Invalid email address";
+  }
+
+  if (!formData.password.trim()) {
+    errors.password = "Password is required";
+  } else if (formData.password < 8) {
+    errors.password = "Invalid password";
+  } else if (!isValidPassword(formData.password)) {
+    errors.password = "Invalid password";
   }
 
   return errors;
