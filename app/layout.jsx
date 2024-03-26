78a3aff30e2950040951037ed6962ebb9a762b32
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-03-26 12:25:17 +0530

    create session provider and import it to root layout

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,9 @@ import { Inter } from "next/font/google";
 import "@/styles/globals.css";
 import Navbar from "@/components/Navbar";
 
+import { getServerSession } from "next-auth";
+import SessionProvider from "@/utils/sessionProvider";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -9,13 +12,18 @@ export const metadata = {
   description: "Add your notes and manage your works easy",
 };
 
-export default function RootLayout({ children }) {
+const RootLayout = async ({ children }) => {
+  const session = await getServerSession();
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
-        {children}
+        <SessionProvider session={session}>
+          <Navbar />
+          {children}
+        </SessionProvider>
       </body>
     </html>
   );
-}
+};
+
+export default RootLayout;
