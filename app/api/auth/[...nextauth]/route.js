7cb2466b6772eae1b536409fa0116fedfd3da219
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-03-26 23:59:57 +0530

    create next auth route

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,71 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import bcrypt from "bcryptjs";
+import User from "@/models/User";
+import connectToDB from "@/utils/db";
+
+const handler = NextAuth({
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        try {
+          await connectToDB();
+
+          const { email, password } = credentials;
+          const user = await User.findOne({ email });
+
+          if (!user) {
+            throw new Error("No user found with this email.");
+          }
+
+          const isPasswordCorrect = await bcrypt.compare(
+            password,
+            user.password
+          );
+
+          if (!isPasswordCorrect) {
+            throw new Error("Incorrect password.");
+          }
+
+          return user;
+        } catch (error) {
+          console.log("Error authenticating user: ", error.message);
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user, session }) {
+      // console.log("jwt callback", { token, user, session });
+      if (user) {
+        return {
+          ...token,
+          id: user._id,
+        };
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      // console.log("session callback", { token, user, session });
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+        },
+      };
+    },
+  },
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: "jwt",
+  },
+});
+
+export { handler as GET, handler as POST };
