1b764f20c960404ef6b685d2f79e2bec9fe67e16
YasithaRDK <yasithadilshanrajapaksha@gmail.com>
2024-03-29 10:40:51 +0530

    change get notes order ton desc

--- app/api/users/[id]/notes/route.js
+++ app/api/users/[id]/notes/route.js
@@ -6,7 +6,9 @@ export const GET = async (request, { params }) => {
   try {
     await connection();
 
-    const notes = await Note.find({ creator: params.id }).sort({ _id: -1 });
+    const notes = await Note.find({ creator: params.id }).sort({
+      createdAt: -1,
+    });
 
     return new NextResponse(JSON.stringify(notes), { status: 200 });
   } catch (error) {
