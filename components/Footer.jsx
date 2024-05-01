7f5f4b2a06c48730e278fb49ce878ee8ae010baa
Yasitha Dilshan Rajapaksha <54005592+YasithaRDK@users.noreply.github.com>
2024-05-01 08:16:36 +0530

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -10,8 +10,7 @@ const Footer = () => {
         <footer className="bottom-0 w-full py-5 mt-10 bg-white border-t border-gray-300">
           <div className="container mx-auto text-center">
             <p className="text-sm">
-              &copy; {new Date().getFullYear()} Created by:{" "}
-              <a href="https://yasitha-dilshan.vercel.app/">Yasitha Dilshan</a>
+              &copy; {new Date().getFullYear()} QuickNotez. All rights reserved.
             </p>
           </div>
         </footer>
